05975a13853a907bc285244c79f3ed4ae41573a6
DAngelo Thomas <dangelo1thomas@gmail.com>
2020-10-29 23:01:45 -0400

    commit tree 5f698b59dcf4aa18833726827f2a3935b3cf36ad

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,63 @@
+def sort_array_asc(array)
 
+   array.sort do |a, b|
+        a <=> b
+        end   
+    
+end 
+
+def sort_array_desc(array)
+    array.sort do |a, b|
+        if a == b
+            0
+        elsif a < b
+            1
+        elsif a > b
+            -1
+        end
+    end
+end
+
+def sort_array_char_count(array)
+    
+    array.sort_by {|name| name.length}
+end
+
+def swap_elements(array)
+
+    array[1], array[2] = array[2], array[1]
+return array
+
+end
+
+def reverse_array(array)
+    array.reverse 
+end
+
+def kesha_maker(array)
+array.each do |name|
+    name[2] = "$"
+end
+end
+
+def find_a(array)
+    
+    array.select {|name| name[0] == "a" }
+    
+end
+
+def sum_array(array)
+    array.inject(0){|sum,x| sum + x}
+
+end
+
+
+def add_s(array)
+    array.each_with_index.collect do |element, index|
+        if element != array[1]
+            element = element + "s"
+        else 
+           element
+        end  
+    end
+end
